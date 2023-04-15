e2df7028a4f2d7c26a6952f0e4b52e0616fd47bf
Luciano Hugo Carvalho De Sousa <luccarva@c1r5s7.42porto.com>
2023-04-16 00:16:36 +0100

    up ft_to*.

--- testes/ft_tolower.c
+++ testes/ft_tolower.c
@@ -6,8 +6,35 @@
 /*   By: luccarva <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 22:45:34 by luccarva          #+#    #+#             */
-/*   Updated: 2023/04/12 22:45:36 by luccarva         ###   ########.fr       */
+/*   Updated: 2023/04/15 23:47:24 by luccarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctype.h>
+#include <stdio.h>
 
+int tolower(int c);
+
+int ft_tolower(unsigned int c)
+{
+	if (c >='A' && c <= 'Z')
+		return (c + 32); 
+	return (c);
+}
+
+int	main()
+{
+	//char	c;
+	int		foo;
+	int		ft;
+
+	//c = '0';
+	foo = tolower('È');
+	ft = ft_tolower('È');
+	printf("__________________\n");
+	//printf("\nnumero: %d\n",i);
+	printf("\texpected: %d\n", foo);
+	printf("\t     got: %d\n", ft);
+	//printf("__________________\n");
+	return (0);
+}
