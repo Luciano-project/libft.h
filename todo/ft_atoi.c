67650bc366b60401cade9f3d6568ea30b4f137bc
Luciano <luhugo.lhcs@gmail.com>
2023-04-21 00:18:56 +0100

    update 1rst part of atoi

--- todo/ft_atoi.c
+++ todo/ft_atoi.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	num;
+	
+	num = 0;
+	while (*nptr)
+	{
+		num += ((const char ) *nptr) - '0';
+		num *= 10;
+		nptr++;
+	}
+	//printf("%d",num);
+	return (num / 10);
+}
+
+int	main(int argc, const char **argv)
+{
+	const char *string;
+	
+	string = argv[argc-1];
+	//printf("%s",string);
+	printf("%d\n",ft_atoi(string));
+	return (0);
+}
